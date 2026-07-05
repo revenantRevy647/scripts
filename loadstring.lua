76c50777e5d09c257e15ab457f7d7338fe5a0275
Revy <screwdriver647@gmail.com>
2026-07-05 06:03:44 +0300

    Update loadstring.lua

--- loadstring.lua
+++ loadstring.lua
@@ -1,1 +1,3 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/revenantRevy647/scripts/refs/heads/main/teleportGui.lua"))()
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/revenantRevy647/scripts/refs/heads/main/hitboxGui.lua"))()
